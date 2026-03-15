4b9814b6401f7fc28184849bb981265b8ab7d7c8
fzsfdc <61556763+fzsfdc@users.noreply.github.com>
2026-03-15 23:30:36 +0100

    styling and beginning of JS

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -1,3 +1,43 @@
 import { LightningElement } from 'lwc';
 
-export default class BmiCalculator extends LightningElement {}
+export default class BmiCalculator extends LightningElement {
+
+  height = '';
+  weight = '';
+
+  /**
+   * 
+   * @param {*} event 
+   * 
+   * Optionally:
+   * this[name]=value;
+   */
+  inputHandler(event) {
+    const {name, value} = event.target;
+
+    if(name==="height") {
+      this.height = value;
+    }
+    if(name==="weight") {
+      this.weight = value;
+    }  
+  }
+
+  /**
+   * 
+   */
+  submitHandler(event) {
+    event.preventDefault();
+    console.log('height: ', this.height);
+    console.log('weight: ', this.weight);
+    /** const bmi = this.weight / ((this.height/100) * (this.height/100)); */
+  }
+
+  calculateBmi() {
+    let height = Number(this.height)/100;
+    let bmi = Number(this.weight) / (height * height);
+    bmi = bmi.toFixed(2);
+    console.log('bmi value: ', bmi);
+
+  }
+}
