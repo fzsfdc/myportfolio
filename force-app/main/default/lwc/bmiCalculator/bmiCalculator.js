0f17796ea32964b636b07245cbbdcb6e24d2e06a
fzsfdc <61556763+fzsfdc@users.noreply.github.com>
2026-03-16 22:37:09 +0100

    Final V1

--- force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
+++ force-app/main/default/lwc/bmiCalculator/bmiCalculator.js
@@ -4,6 +4,8 @@ export default class BmiCalculator extends LightningElement {
 
   height = '';
   weight = '';
+  bmiValue= '';
+  result='';
 
   /**
    * 
@@ -31,13 +33,39 @@ export default class BmiCalculator extends LightningElement {
     console.log('height: ', this.height);
     console.log('weight: ', this.weight);
     /** const bmi = this.weight / ((this.height/100) * (this.height/100)); */
+    this.calculateBmi();
   }
 
+  /**
+   * 
+   */
   calculateBmi() {
     let height = Number(this.height)/100;
     let bmi = Number(this.weight) / (height * height);
-    bmi = bmi.toFixed(2);
-    console.log('bmi value: ', bmi);
+    this.bmiValue = Number(bmi.toFixed(2));
+    // console.log('bmi value: ', bmi);
 
+    if(this.bmiValue < 18.5) {
+      this.result = "Underweight";
+    } else if(this.bmiValue >= 18.5 && this.bmiValue < 25) {
+      this.result = "Healthy";
+    } else if(this.bmiValue >= 25 && this.bmiValue < 30) {
+      this.result = "Overweight";
+    } else {
+      this.result = "Obese";
+    } 
+
+    console.log('result: ', this.result);
+    console.log('bmi value: ', this.bmiValue);
+  }
+
+  /**
+   * 
+   */
+  recalculate() {
+    this.height = '';
+    this.weight = '';
+    this.bmiValue= '';
+    this.result='';
   }
 }
