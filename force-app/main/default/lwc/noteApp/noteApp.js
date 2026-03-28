9b4b7431f73caa0d827c9bcf5efa8cad049f7645
fzsfdc <61556763+fzsfdc@users.noreply.github.com>
2026-03-28 18:03:29 +0100

    Updates on NoteApp

--- force-app/main/default/lwc/noteApp/noteApp.js
+++ force-app/main/default/lwc/noteApp/noteApp.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
 
-export default class NoteApp extends LightningElement {}
+
+const DEFAULT_NOTE_FORM = {
+    Name:"",
+    Note_Description__c:""
+}
+
+export default class NoteApp extends LightningElement {
+    showModal = false;
+    noteRecord=DEFAULT_NOTE_FORM;
+    formats = ['font','size','bold','italic','underline','strike','list','indent','align','link','clean','table','header','color'];
+
+    createNoteHandler() {
+        this.showModal = true;
+    }
+
+    closeModalHandler() {
+        this.showModal = false;
+        this.noteRecord = DEFAULT_NOTE_FORM;
+    }
+
+    changeHandler(event) {
+        const{nme,value} = event.target;
+        //const name = event.target.name;
+        //const value = event.target.value;
+        this.noteRecord = {...this.noteRecord, [name]:value};
+    }
+
+    formSubmitHandler(event) {
+        event.preventDefault();
+        console.log("this.noteRecord: ", this.noteRecord);
+
+    }
+}
